0c6597fbe7f381713dd223c53fea5c878f271135
Team Odoo NSPL <odoo_team@namahsoftech.com>
2025-08-07 16:29:04 +0530

    Add files via upload

---
+++ nspl_bom_product_cost_price/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'BOM Product Cost Price',
+    'version': '17.0',
+    'summary': 'Show cost price of BoM product and total BoM cost',
+    'description': """
+    Display cost price of the product defined in the Bill of Materials (BoM) and the total cost of all components.
+
+    ✔ View BoM component cost and total cost  
+    ✔ Improve production cost transparency  
+    ✔ Assist in pricing and margin calculations
+    """,
+    'category': 'Manufacturing',
+    'sequence': 4,
+    'author': 'Namah Softech Private Limited',
+    'website': 'https://www.nspl.com',
+    'license': 'LGPL-3',
+    'support': 'support@namahsoftech.com',
+    'price': 19.99,
+    'currency': 'USD',
+    'contributors': ['Shivani Solanki'],
+    'depends': ['mrp', 'product'],
+    'data': [
+        'views/mrp_bom_views.xml',
+        'views/product_views.xml',
+    ],
+    'images': ['static/description/img/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
